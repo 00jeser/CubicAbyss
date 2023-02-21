3686b36b023966633de08830e0d75247c6dca6e7
asddd <xafizoff.raf@yandex.ru>
2023-02-22 01:06:45 +0500

    Ya nasral

---
+++ src/main/java/LuboeBlok.java
@@ -0,0 +1,9 @@
+import net.minecraft.block.Block;
+import net.minecraft.block.material.MapColor;
+import net.minecraft.block.material.Material;
+
+public class LuboeBlok extends Block {
+    public LuboeBlok(Material p_i46399_1_, MapColor p_i46399_2_) {
+        super(p_i46399_1_, p_i46399_2_);
+    }
+}
