0142f631f7ba845a10fc436c09a6be33aee1bc13
OOjeser <boss.gavrilenko@bk.ru>
2023-02-21 15:40:23 +0500

    add layers

--- src/main/java/jeser/cubicabyss/world/generator/AbyssWorldGenerator.java
+++ src/main/java/jeser/cubicabyss/world/generator/AbyssWorldGenerator.java
@@ -3,17 +3,18 @@ package jeser.cubicabyss.world.generator;
 import io.github.opencubicchunks.cubicchunks.api.util.IntRange;
 import io.github.opencubicchunks.cubicchunks.api.world.ICubicWorldType;
 import io.github.opencubicchunks.cubicchunks.api.worldgen.ICubeGenerator;
+import net.minecraft.init.Biomes;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldServer;
 import net.minecraft.world.WorldType;
-import net.minecraft.world.chunk.IChunkProvider;
-import net.minecraft.world.gen.IChunkGenerator;
-import net.minecraftforge.fml.common.IWorldGenerator;
-
-import javax.annotation.Nullable;
-import java.util.Random;
+import net.minecraft.world.biome.BiomeProvider;
+import net.minecraft.world.biome.BiomeProviderSingle;
+import net.minecraftforge.common.DimensionManager;
 
 public class AbyssWorldGenerator  extends WorldType implements ICubicWorldType {
+    public static final float RADIUS = 100;
 
     public static AbyssWorldGenerator create ()
     {
@@ -21,20 +22,36 @@ public class AbyssWorldGenerator  extends WorldType implements ICubicWorldType {
     }
     public AbyssWorldGenerator() {
         super("aughauua");
+
+
     }
 
     @Override
     public IntRange calculateGenerationHeightRange(WorldServer worldServer) {
-        return null;
+        return new IntRange(-1000, 200);
     }
 
     @Override
     public boolean hasCubicGeneratorForWorld(World world) {
-        return false;
+        return world.provider.getClass() == DimensionManager.getProvider(0).getClass();
     }
 
     @Override
     public ICubeGenerator createCubeGenerator(World world) {
         return new CustomTerrainGenerator(world, world.getSeed());
     }
+
+
+    @Override
+    public BiomeProvider getBiomeProvider(World world) {
+        return new BiomeProviderSingle(Biomes.PLAINS);
+    }
+
+    @Override
+    public int getSpawnFuzz(WorldServer world, MinecraftServer server) {
+        world.setSpawnPoint(new BlockPos(102, 64, 0));
+        return 11;
+    }
+
+
 }
