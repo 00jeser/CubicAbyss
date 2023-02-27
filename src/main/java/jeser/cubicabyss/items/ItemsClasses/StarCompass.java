a05c626fc114af3e1a2b520dcf3eb2debd055e76
OOjeser <boss.gavrilenko@bk.ru>
2023-02-27 10:33:34 +0500

    Добавлены стихии для артефактов

--- src/main/java/jeser/cubicabyss/items/ItemsClasses/StarCompass.java
+++ src/main/java/jeser/cubicabyss/items/ItemsClasses/StarCompass.java
@@ -1,12 +1,17 @@
 package jeser.cubicabyss.items.ItemsClasses;
 
+import jeser.cubicabyss.items.Artefact;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.Item;
 
-public class StarCompass extends Item {
+public class StarCompass extends Artefact {
     public StarCompass() {
         super();
         setRegistryName("star_compass");
         this.setCreativeTab(CreativeTabs.MATERIALS);
+    }
+
+    @Override
+    public Artefact.Elementals getElement() {
+        return Artefact.Elementals.stone;
     }
 }
