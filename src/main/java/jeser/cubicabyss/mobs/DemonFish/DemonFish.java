da810fc23788d9a88ef5718d7fffcfffc9994635
Nuuudelholz <stolyarow200138@gmail.com>
2023-03-11 22:41:18 +0500

    Добавлен моб рыба-демон

--- src/main/java/jeser/cubicabyss/mobs/DemonFish/DemonFish.java
+++ src/main/java/jeser/cubicabyss/mobs/DemonFish/DemonFish.java
@@ -7,18 +7,19 @@ import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.*;
 import net.minecraft.entity.monster.EntityGuardian;
-import net.minecraft.entity.passive.EntityAnimal;
-import net.minecraft.entity.passive.EntitySquid;
-import net.minecraft.entity.passive.EntityWaterMob;
+import net.minecraft.entity.passive.*;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.init.SoundEvents;
+import net.minecraft.pathfinding.PathFinder;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.world.World;
+import net.minecraftforge.client.model.animation.Animation;
 import net.minecraftforge.fml.common.registry.EntityRegistry;
 import software.bernie.geckolib3.GeckoLib;
+import software.bernie.geckolib3.core.AnimationState;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
 import software.bernie.geckolib3.core.builder.AnimationBuilder;
@@ -30,7 +31,8 @@ import software.bernie.geckolib3.util.GeckoLibUtil;
 
 import javax.annotation.Nullable;
 
-public class DemonFish extends EntitySquid implements IAnimatable {
+public class DemonFish extends EntityGuardian implements IAnimatable {
+    public float lasthp = this.getHealth();
     private AnimationFactory factory = new AnimationFactory(this);
     public DemonFish(World worldIn) {
         super(worldIn);
@@ -54,14 +56,30 @@ public class DemonFish extends EntitySquid implements IAnimatable {
         this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(5.0D);
     }
     private <E extends IAnimatable>PlayState predicate(AnimationEvent<E> event) {
-
+        lasthp = this.getHealth();
+        if (lasthp > this.getHealth()){
+            event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.model.hurt", false));
+            return PlayState.CONTINUE;
+        }
+        if (event.getController().getAnimationState() == AnimationState.Running)
+            return PlayState.CONTINUE;
         if (event.isMoving()) {
             event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.model.move", true));
-            //System.out.println(1);
             return PlayState.CONTINUE;
         }
         event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.model.idle", true));
         return PlayState.CONTINUE;
+    }
+    @Override
+    protected void initEntityAI() {
+        EntityAIMoveTowardsRestriction entityaimovetowardsrestriction = new EntityAIMoveTowardsRestriction(this, 1.0D);
+        this.wander = new EntityAIWander(this, 1.0D, 80);
+        this.tasks.addTask(5, entityaimovetowardsrestriction);
+        this.tasks.addTask(7, this.wander);
+        this.tasks.addTask(8, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
+        this.tasks.addTask(8, new EntityAIWatchClosest(this, EntityGuardian.class, 12.0F, 0.01F));
+        this.tasks.addTask(9, new EntityAILookIdle(this));
+        this.wander.setMutexBits(3);
     }
 
     @Override
